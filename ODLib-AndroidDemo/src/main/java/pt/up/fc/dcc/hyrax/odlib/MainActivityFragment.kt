9426107a22051e76adc9da004ad3acd57b3c4799
Joaquim Silva <jmes@dcc.fc.up.pt>
2018-09-27 12:29:44 +0100

    Added Android App

---
+++ ODLib-AndroidDemo/src/main/java/pt/up/fc/dcc/hyrax/odlib/MainActivityFragment.kt
@@ -0,0 +1,25 @@
+package pt.up.fc.dcc.hyrax.odlib
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var lib = ODLib(context!!)
+        lib.startODService()
+        lib.getClient()
+    }
+}
